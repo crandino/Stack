59dd554f1fa5983522061382ac63592b026851c8
crandino <carlosmanbe@hotmail.com>
2015-04-12 12:33:50 +0200

    Stack class rewritten

--- Stack.h
+++ Stack.h
@@ -1,144 +1,89 @@
 #ifndef __STACK_H__
 #define __STACK_H__
 
-#include <assert.h> 
+#include <assert.h>
+#include <stdio.h>
 
 template <class TYPE>
 class Stack {
 
 private:
 
-	TYPE *data;
-	unsigned int allocated_memory;
 	unsigned int num_elements;
+	unsigned int allocated_memory;
+	TYPE *data;
 
-	void reallocate(unsigned int _new_mem_size)
+	void reallocate(unsigned int new_memory)
 	{
 		if (data != NULL)
 		{
-			TYPE *tmp = new TYPE[allocated_memory];
-			for (unsigned int i = 0; i < num_elements; i++)
+			if (num_elements <= new_memory)
 			{
-				tmp[i] = data[i];
-			}
-			delete[] data;
-			allocated_memory = _new_mem_size;
-			data = new TYPE[allocated_memory];
-			for (unsigned int i = 0; i < num_elements; i++)
-			{
-				data[i] = tmp[i];
+				TYPE *tmp = new TYPE[allocated_memory];
+				for (int index = 0; index < num_elements; index++)
+				{
+					tmp[index] = data[index];
+				}
+				delete[] data;
+				allocated_memory = new_memory;
+				data = new TYPE[allocated_memory];
+				for (int index = 0; index < num_elements; index++)
+				{
+					data[index] = tmp[index];
+				}
 			}
 		}
 		else
 		{
-			delete[] data;
-			allocated_memory = _new_mem_size;
+			allocated_memory = new_memory;
 			data = new TYPE[allocated_memory];
 		}
 	}
 
 public:
 
-	Stack<TYPE>() : data(NULL), allocated_memory(0), num_elements(0) {}
-	Stack<TYPE>(unsigned int new_memory_size) : data(NULL), num_elements(0) {
-		reallocate(new_memory_size);
+	Stack<TYPE>() : data(NULL), num_elements(0), allocated_memory(0)
+	{ }
+
+	Stack<TYPE>(unsigned int new_memory) : data(NULL), num_elements(0)
+	{
+		reallocate(new_memory);
 	}
 
-	~Stack<TYPE>() { if (data != NULL) delete[] data; }
+	~Stack<TYPE>()
+	{
+		if (data != NULL) delete data;
+	}
 
 	void push(TYPE new_value)
 	{
-		if (num_elements == allocated_memory)
+		if (data != NULL)
+		{
+			if (num_elements >= allocated_memory)
+				reallocate(allocated_memory + 1);
+		}
+		else
+		{
 			reallocate(allocated_memory + 1);
+		}
 
 		data[num_elements] = new_value;
 		num_elements++;
 	}
 
-	// PEEK.
-	// Permet consultar qualsevol valor de l'array, però no modificarla.
-	const TYPE& peek(unsigned int position) const
-	{
-		assert(position < num_elements);
-		return data[position];
-	}
-
-	// Pasar a booleano y que pida una referencia para llenar con el valor que devuelve.
 	TYPE pop()
 	{
-		// When the element is deleted, it is necessary to return a copy of that element.
-		if (data != NULL && num_elements != 0)
-		{
-			num_elements--;
-			TYPE element_to_return = data[num_elements];
-			return element_to_return;
-		}
-		return -1;
+		TYPE ret = -1;
+		if (data != NULL && num_elements > 0)
+			ret = data[--num_elements];
+		
+		return ret;
 	}
 
-	/*bool insert(int new_value, unsigned int position)
+	const TYPE &peek(unsigned int position) const
 	{
-		if (position <= num_elements)
-		{
-			TYPE *tmp = new TYPE[allocated_memory];
-			for (unsigned int i = 0; i < num_elements; i++)
-			{
-				tmp[i] = data[i];
-			}
-
-			if (num_elements == allocated_memory)
-				reallocate(allocated_memory + 1);
-
-			for (unsigned int i = 0; i < position; i++)
-			{
-				data[i] = tmp[i];
-			}
-			data[position] = new_value;
-			for (unsigned int i = position; i < num_elements; i++)
-			{
-				data[i + 1] = tmp[i];
-			}
-			num_elements++;
-			delete[] tmp;
-			return true;
-		}
-		return false;
-	}*/
-
-	bool clear()
-	{
-		if (data != NULL)
-		{
-			num_elements = 0;
-			return true;
-		}
-		return false;
-	}
-
-	// Auxiliar method useful to represent all the information.
-	// It won't have its correspoding Unittest test. 
-	void info() const
-	{
-		for (int i = 0; i < num_elements; i++)
-		{
-			printf("%s %d => %d\n", "Position", i, data[i]);
-		}
-		printf("%s: %d\n", "Number of elements", num_elements);
-		printf("%s: %d\n\n", "Allocated memory", allocated_memory);
-	}
-
-	int& operator[] (unsigned int index)
-	{
-		// For p[1] = 15;
-		assert(index < num_elements);
-		return data[index];
-	}
-
-	const int& operator[] (unsigned int index) const
-	{
-		// For printf("%d", p[1]);
-		assert(index < num_elements);
-		return data[index];
+		assert(data != NULL && position < count());
+		return data[position];		
 	}
 
 	unsigned int getMemory() const
@@ -146,11 +91,160 @@ public:
 		return allocated_memory;
 	}
 
-	unsigned int getNumElements() const
+	unsigned int count() const
 	{
 		return num_elements;
 	}
+
 };
 
-
 #endif // !__STACK_H__
+
+//private:
+//
+//	TYPE *data;
+//	unsigned int allocated_memory;
+//	unsigned int num_elements;
+//
+//	void reallocate(unsigned int _new_mem_size)
+//	{
+//		if (data != NULL)
+//		{
+//			TYPE *tmp = new TYPE[allocated_memory];
+//			for (unsigned int i = 0; i < num_elements; i++)
+//			{
+//				tmp[i] = data[i];
+//			}
+//			delete[] data;
+//			allocated_memory = _new_mem_size;
+//			data = new TYPE[allocated_memory];
+//			for (unsigned int i = 0; i < num_elements; i++)
+//			{
+//				data[i] = tmp[i];
+//			}
+//		}
+//		else
+//		{
+//			delete[] data;
+//			allocated_memory = _new_mem_size;
+//			data = new TYPE[allocated_memory];
+//		}
+//	}
+//
+//public:
+//
+//	Stack<TYPE>() : data(NULL), allocated_memory(0), num_elements(0) {}
+//	Stack<TYPE>(unsigned int new_memory_size) : data(NULL), num_elements(0) {
+//		reallocate(new_memory_size);
+//	}
+//
+//	~Stack<TYPE>() { if (data != NULL) delete[] data; }
+//
+//	void push(TYPE new_value)
+//	{
+//		if (num_elements == allocated_memory)
+//			reallocate(allocated_memory + 1);
+//
+//		data[num_elements] = new_value;
+//		num_elements++;
+//	}
+//
+//	// PEEK.
+//	// Permet consultar qualsevol valor de l'array, però no modificarla.
+//	const TYPE& peek(unsigned int position) const
+//	{
+//		assert(position < num_elements);
+//		return data[position];
+//	}
+//
+//	// Pasar a booleano y que pida una referencia para llenar con el valor que devuelve.
+//	TYPE pop()
+//	{
+//		// When the element is deleted, it is necessary to return a copy of that element.
+//		if (data != NULL && num_elements != 0)
+//		{
+//			num_elements--;
+//			TYPE element_to_return = data[num_elements];
+//			return element_to_return;
+//		}
+//		return -1;
+//	}
+//
+//	/*bool insert(int new_value, unsigned int position)
+//	{
+//		if (position <= num_elements)
+//		{
+//			TYPE *tmp = new TYPE[allocated_memory];
+//			for (unsigned int i = 0; i < num_elements; i++)
+//			{
+//				tmp[i] = data[i];
+//			}
+//
+//			if (num_elements == allocated_memory)
+//				reallocate(allocated_memory + 1);
+//
+//			for (unsigned int i = 0; i < position; i++)
+//			{
+//				data[i] = tmp[i];
+//			}
+//			data[position] = new_value;
+//			for (unsigned int i = position; i < num_elements; i++)
+//			{
+//				data[i + 1] = tmp[i];
+//			}
+//			num_elements++;
+//			delete[] tmp;
+//			return true;
+//		}
+//		return false;
+//	}*/
+//
+//	bool clear()
+//	{
+//		if (data != NULL)
+//		{
+//			num_elements = 0;
+//			return true;
+//		}
+//		return false;
+//	}
+//
+//	// Auxiliar method useful to represent all the information.
+//	// It won't have its correspoding Unittest test. 
+//	void info() const
+//	{
+//		for (int i = 0; i < num_elements; i++)
+//		{
+//			printf("%s %d => %d\n", "Position", i, data[i]);
+//		}
+//		printf("%s: %d\n", "Number of elements", num_elements);
+//		printf("%s: %d\n\n", "Allocated memory", allocated_memory);
+//	}
+//
+//	int& operator[] (unsigned int index)
+//	{
+//		// For p[1] = 15;
+//		assert(index < num_elements);
+//		return data[index];
+//	}
+//
+//	const int& operator[] (unsigned int index) const
+//	{
+//		// For printf("%d", p[1]);
+//		assert(index < num_elements);
+//		return data[index];
+//	}
+//
+//	unsigned int getMemory() const
+//	{
+//		return allocated_memory;
+//	}
+//
+//	unsigned int getNumElements() const
+//	{
+//		return num_elements;
+//	}
+//};
+//
+//
+//#endif // !__STACK_H__
--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -10,38 +10,81 @@ namespace UnitTest1
 	TEST_CLASS(UnitTest1)
 	{
 	public:
-		
+
 		TEST_METHOD(StackGeneralTest)
 		{
 			// We test various codes.
-			Stack<int> s(5);
+			Stack<int> s5(5);
+			Stack<float> s0;
+			
+			s5.push(1);
+			s5.push(2);
+			s5.push(3);
+			s5.push(4);
+			s5.push(5);
+			s5.push(6);
 
-			s.push(1);
-			s.push(2);
-			s.push(3);
+			Assert::AreEqual(s5.peek(0), 1);
+			Assert::AreEqual(s5.peek(1), 2);
+			Assert::AreEqual(s5.peek(2), 3);
+			Assert::AreEqual(s5.peek(4), 5);
+			Assert::AreEqual(s5.peek(5), 6);
+			
+			Assert::AreEqual(s5.pop(), 6);
+			Assert::AreEqual(s5.pop(), 5);
+			Assert::AreEqual(s5.pop(), 4);
+			Assert::AreEqual(s5.pop(), 3);
+			Assert::AreEqual(s5.pop(), 2);
+			Assert::AreEqual(s5.pop(), 1);
+			Assert::AreEqual(s5.pop(), -1);
 
-			Assert::AreEqual(s.peek(2), 3);
+			s0.push(1.0f);
+			s0.push(2.0f);
+			s0.push(3.0f);
+			s0.push(4.0f);
+		
+			Assert::AreEqual(s0.pop(), 4.0f);
+			Assert::AreEqual(s0.pop(), 3.0f);
+			Assert::AreEqual(s0.pop(), 2.0f);
+			Assert::AreEqual(s0.pop(), 1.0f);
+			Assert::AreEqual(s0.pop(), -1.0f);
 
-			Assert::AreEqual(s.pop(), 3);
-			Assert::AreEqual(s.pop(), 2);
-			Assert::AreEqual(s.pop(), 1);
+
+
+
 		}
 
-		TEST_METHOD(QueueGeneralTest)
-		{
-			// We test various codes.
-			Queue<int> s;
+		//TEST_METHOD(StackGeneralTest)
+		//{
+		//	// We test various codes.
+		//	Stack<int> s(5);
 
-			s.push(1);
-			s.push(2);
-			s.push(3);
+		//	s.push(1);
+		//	s.push(2);
+		//	s.push(3);
 
-			Assert::AreEqual(s.peek(2), 3);
+		//	Assert::AreEqual(s.peek(2), 3);
 
-			Assert::AreEqual(s.pop(), 1);
-			Assert::AreEqual(s.pop(), 2);
-			Assert::AreEqual(s.pop(), 3);
-		}
+		//	Assert::AreEqual(s.pop(), 3);
+		//	Assert::AreEqual(s.pop(), 2);
+		//	Assert::AreEqual(s.pop(), 1);
+		//}
+
+		//TEST_METHOD(QueueGeneralTest)
+		//{
+		//	// We test various codes.
+		//	Queue<int> s;
+
+		//	s.push(1);
+		//	s.push(2);
+		//	s.push(3);
+
+		//	Assert::AreEqual(s.peek(2), 3);
+
+		//	Assert::AreEqual(s.pop(), 1);
+		//	Assert::AreEqual(s.pop(), 2);
+		//	Assert::AreEqual(s.pop(), 3);
+		//}
 
 	};
 }
